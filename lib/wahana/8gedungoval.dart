2d302c00863df1fc0b7cdf3911617a5088ce4027
Nur Fajar Ismail <nur.fajar.ft18@mail.umy.ac.id>
2021-01-04 16:17:27 +0700

    add wahan 8 akhir

--- lib/wahana/8gedungoval.dart
+++ lib/wahana/8gedungoval.dart
@@ -227,12 +227,192 @@ class WahanaDelapan extends StatelessWidget {
                       ),
                     ),
                   ),
+                ],
+              ),
+            ),
+            Container(
+              child: Text("Zona Tata Surya"),
+            ),
+            Container(
+              child: RichText(
+                textAlign: TextAlign.justify,
+                text: TextSpan(
+                  text: "Ini adalah tempat dimana pengunjung dapat",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: '  mempelajari mengenai tata surya kita.',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    TextSpan(
+                      text:
+                          '  Dalam zona ini juga telah dilengkapi dengan ICT sehingga pengunjung dapat “berpetualang” ke sebuah planet dalam system tata surya dan menggali pengetahuan dari planet yang dikunjungi tersebut.',
+                      style: TextStyle(
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              child: Text(
+                "Kegiatan",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              child: GridView.count(
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        'assets/images/gedungoval9.png',
+                        width: 110.0,
+                        height: 110.0,
+                      ),
+                    ),
+                  ),
                   Container(
                     margin: EdgeInsets.all(10),
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(20),
                       child: Image.asset(
                         'assets/images/gedungoval8.png',
+                        width: 110.0,
+                        height: 110.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Text("Lorong Ilusi"),
+            ),
+            Container(
+              child: RichText(
+                textAlign: TextAlign.justify,
+                text: TextSpan(
+                  text:
+                      " Ini adalah sebuah Lorong yang dapat membingungkan mata, hal ini terjadi karena kesalahan penangkapan mata manusia.",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text:
+                          '  Terdapat 2 jenis ilusi, yaitu Ilusi Fisiologis adalah kesan gambar yang terjadi setelah melihat cahaya yang sangat terang atau melihat pola gambar tertentu dalam waktu lama, ini diduga merupakan efek yang terjadi pada mata atau otak setelah mendapat rangsangan tertentu secara berlebihan. Ada juga Ilusi Kognitif, diasumsikan terjadi karena anggapan pikiran terhadap sesuatu diluar gambar itu sendiri.',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              child: Text(
+                "Kegiatan",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              child: GridView.count(
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        'assets/images/gedungoval10.png',
+                        width: 110.0,
+                        height: 110.0,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        'assets/images/gedungoval11.png',
+                        width: 110.0,
+                        height: 110.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Text("Zona Kehidupan Purba"),
+            ),
+            Container(
+              child: RichText(
+                textAlign: TextAlign.justify,
+                text: TextSpan(
+                  text:
+                      "Salah satu kehidupan purba yang ditampilkan dalam zona ini adalah binatang purba yang hidup sekitar 250 juta tahun lalu yaitu Dinosaurus.",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text:
+                          '  Dinosaurus adalah sebutan untuk binatang purba yang telah punah, hingga kini kita hanya dapat memperkirakan bentuknya, suaranya, ukurannya berdasarkan penelitian para ahli dan fosil fosil yang ditinggalkan. Dalam zona ini terdapat peraga robotic sehingga dinosaurus tersebut dapat memutar kepala, membuka mulut dan mengeluarkan suara.',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              child: Text(
+                "Kegiatan",
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              child: GridView.count(
+                physics: NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        'assets/images/gedungoval12.png',
+                        width: 110.0,
+                        height: 110.0,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.asset(
+                        'assets/images/gedungoval13.png',
                         width: 110.0,
                         height: 110.0,
                       ),
